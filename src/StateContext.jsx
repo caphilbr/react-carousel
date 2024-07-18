b2562e1bb2ff15cef98f833598696cc9ad76b015
caphilbr <caphilbr@hotmail.com>
2024-07-18 14:53:57 -0400

    carousel implemented for car list

--- src/StateContext.jsx
+++ src/StateContext.jsx
@@ -12,6 +12,9 @@ export const StateWrapper = ({ children }) => {
       year: "2023",
     },
     { id: 2, make: "Honda", model: "Odyssey", year: "2018" },
+    { id: 3, make: "Honda", model: "Odyssey", year: "2019" },
+    { id: 4, make: "Honda", model: "Odyssey", year: "2020" },
+    { id: 5, make: "Honda", model: "Odyssey", year: "2021" }
   ]
   const [cars, setCars] = useState(seedCars)
   const [dealerCount, setDealerCount] = useState(0)
--- src/styling/CarTile.js
+++ src/styling/CarTile.js
@@ -4,11 +4,9 @@ const CarTile = styled.div`
   background-color: white;
   color: black;
   padding: 15px 32px;
-  display: inline-block;
-  width: 35%;
   border-radius: 0.5rem;
   text-align: left;
   font-size: 16px;
-  margin: 4px 4px;
+  margin: 4px 2px;
 `
-export default CarTile
+export default CarTile  
